3fd7f7861378a0994ebe124ef4c9013124e6723b
Jyeiii <84722465+Jyeiii@users.noreply.github.com>
2022-12-09 13:18:01 +0800

    Add files via upload

---
+++ Coffee Shop.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="UTF-8">
+	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	 <meta name="viewport" content="width=device=width, initial-scale=1.0">
+	 <link rel="stylesheet" href="style.css">
+	 <title> Sumaki Junji | Coffee Shop</title>
+</head>
+<body>
+
+	    <div class="container"> 
+		<div class="nav-bar">   
+		
+		<h1 class="title">Sum<span>aki</span></h1>
+		
+		<p>Coffee Shop</p>
+		<ul class="menu">
+		    <li><a href="#">HOME</a></li>
+			<li><a href="#">PRODUCT</a></li>
+			<li><a href="#">BLOG</a></li>
+			<li><a href="#">CONTACT</a></li>
+			<li><a href="#">SERVICES</a></li>
+			<li><a href="#">ABOUT</a></li>
+			</ul>
+		</div>
+		<div class="home">
+		     <h1 class="title-1">May your<span> Good Coffee</span>  kick in<br>before reality does!</h1>
+			 <p> We provide a variety of uniques and Best Coffee</p>
+			 <button type="button">Shop Now</button>
+		</div>
+		</div>
+		<div class="copyright">
+	     <p align="center">Copyright &copy; 2020-2022 SumakiCoffeeShop.com</p>
+		</div>
+</body>
+</html>			
