db20af77ed1f592bf57ca32cb573fbc8dec3ad4d
Jyeiii <84722465+Jyeiii@users.noreply.github.com>
2022-12-13 21:11:32 +0800

    Add files via upload

---
+++ Ramen Shop.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <!-- Metas -->
+     <meta charset="UTF-8">
+	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	 <meta name="viewport" content="width=device=width, initial-scale=1.0">
+	 
+	 
+	 <!-- Stylesheets -->
+	 <link rel="stylesheet" href="style.css">
+	 <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+	 <link rel="stylesheet"
+     href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+	 <title> Sumaki Junji | Ramen Shop</title>
+	 
+	  
+</head>
+<body>
+    
+
+	    <div class="container"> 
+		<div class="nav-bar">  
+		<h1 class="title">Sum<span>aki</span></h1>
+		<p>Ramen Shop</p>
+	
+		<ul class="menu">
+		    <li><a href="#">HOME</a></li>
+			<li><a href="#">PRODUCT</a></li>
+			<li><a href="#">CONTACT</a></li>
+			<li><a href="#">ABOUT</a></li>
+			</ul>
+		</div>
+		<div class="home">
+		     <h1 class="title-1">May your<span> Good Ramen</span>  kick in<br>before reality does!</h1>
+			 <p> We provide a variety of uniques and Best Ramen</p>
+			 <button type="button">Shop Now</button>
+		</div>
+		</div>
+		<div class="copyright">
+	     <p align="center">Copyright &copy; 2020-2022 SumakiRamenShop.com</p>
+		</div>
+
+</body>
+</html>			
