7b8d5f45e46b56ec611bb5355a212a85532df3c0
Jyeiii <84722465+Jyeiii@users.noreply.github.com>
2023-09-12 17:00:56 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace LAB_2_CAL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Lime;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button7.Location = new System.Drawing.Point(149, 111);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(133, 71);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "Connect";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Crimson;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(402, 111);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(133, 71);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "Disconnect";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(54, 239);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(133, 71);
+            this.button9.TabIndex = 2;
+            this.button9.Text = "Mode 1";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(208, 239);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(133, 71);
+            this.button10.TabIndex = 3;
+            this.button10.Text = "Mode 2";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(362, 239);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(133, 71);
+            this.button11.TabIndex = 4;
+            this.button11.Text = "Mode 3";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(517, 239);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(133, 71);
+            this.button12.TabIndex = 5;
+            this.button12.Text = "Mode 4";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 368);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Mode Status :";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(696, 504);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.IO.IOexception;
+
+namespace LAB_2_CAL
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort = new SerialPort();
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort = new SerialPort();
+            serialPort.BaudRate = 9600;
+            serialPort.PortName = "COM9";
+            serialPort.Open();
+            serialPort.Parity = Parity.None;
+            serialPort.DataBits = 8;
+            serialPort.StopBits = StopBits.One;
+            serialPort.Handshake = Handshake.None;
+            Label label1 = new Label();
+            // Set the label's text.
+            label1.Location = new Point(10, 10);
+            // Add the label to the form.
+            this.Controls.Add(label1);
+        }
+
+     
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // Send the "connect" command to the Arduino.
+            serialPort.WriteLine("CONNECT");
+            // Update the label to show the current mode.
+            label1.Text = "Mode: Connect";
+          
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            // Send the "mode1" command to the Arduino.
+            serialPort.WriteLine("Mode 2");
+            // Update the label to show the current mode.
+            label1.Text = "Mode: 2";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            // Send the "mode1" command to the Arduino.
+            serialPort.WriteLine("Mode 3");
+            // Update the label to show the current mode.
+            label1.Text = "Mode: 3";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            // Send the "mode1" command to the Arduino.
+            serialPort.WriteLine("Mode 4");
+            // Update the label to show the current mode.
+            label1.Text = "Mode: 4";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            // Send the "disconnect" command to the Arduino.
+            serialPort.WriteLine("disconnect");
+            // Update the label to show the current mode.
+            label1.Text = "Mode: Disconnect";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            // Send the "mode1" command to the Arduino.
+            serialPort.WriteLine("Mode 1");
+            // Update the label to show the current mode.
+            label1.Text = "Mode: 1";
+        }
+    }
+
+
+
+
+
+
+      
+  
+}
